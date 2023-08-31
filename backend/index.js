73c7843361ff6d991db006ceb45ae5ed64ed8558
Neufeld <vn@rodenbergmail.de>
2023-08-31 08:50:20 +0200

    env

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./routes/router')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
     origin: '*',
@@ -18,7 +20,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use('/', router);
 
-const port = 4000
+
+const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(process.env.MONGODB_URI, dbOptions)
+    .then(() => console.log('DB connection established'))
+    .catch(err => console.error(err))
+
+const port = process.env.PORT || 4000
 const server = app.listen(port, () => {
     console.log('listening on port ' + port)
 })
