5f523350a3d479fb45b3ac6feb0051133a2276cb
Neufeld <vn@rodenbergmail.de>
2023-09-27 13:43:10 +0200

    style tournaments

--- src/components/TournamentsDB.jsx
+++ src/components/TournamentsDB.jsx
@@ -16,6 +16,14 @@ function TournamentsDB() {
         setTournaments(tournaments);
     };
 
+    const humanReadableDate = (date) => {
+        const data = new Date(date).toLocaleDateString('de-DE',{
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'})
+        return data;
+    };
+
     const [hoveredIndex, setHoveredIndex] = useState(null);
 
     const handleHover = (index) => {
@@ -40,31 +48,28 @@ function TournamentsDB() {
     return (
         <>
             <div className="container">
-                <div className='row row-cols-1 row-cols-md-2 mt-5'>
+                <div className='row row-cols-1 row-cols-md-3 mt-5'>
                     {tournaments && tournaments.reverse().map((data, index) => (
-                        <div className="col col-lg-3 col-md-6 mb-4" key={index}>
-
+                        <div className="col col-lg-4 col-md-6 mb-4" key={index}>
                             <div className="card mx-2 h-100 d-flex flex-column" onMouseEnter={() => handleHover(index)} onMouseLeave={handleLeave}>
                                 {hoveredIndex === index && (
                                     <div className="position-absolute" style={{ top: '5px', right: '5px' }}>
                                         <button className="btn btn-danger btn-sm" onClick={(event) => handleDelete(event, index)}>X</button>
                                     </div>
                                 )}
-                                <img className="card-img-top" src={`/img/${data.sport + '.jpg'}`} alt="Card cap" />
-                                <div className="card-body">
-                                    <Link to={`/singleTournamentDB/${tournaments[index]._id.toString()}`} className="text-decoration-none">
-
-                                        <div className='text-decoration-none'>
-                                            <h2> {_.capitalize(data.name)} Turnier </h2>
-                                            <p><strong>Veranstalter:</strong> {data.organizer}</p>
-                                            <p><strong>Ort:</strong> {data.location}</p>
-                                            <p><strong>Datum:</strong> {data.date}</p>
-                                            <p><strong>Anzahl Mannschaften:</strong> {data.amount}</p>
+                                <img className="card-img-top" src={`/img/${data.name + '.jpg'}`} alt="Card cap" />
+                                <div className="card-body content ">
+                                    <Link to={`/tournament/${index}`}>
+                                        <div className='text-decoration-none '>
+                                            <h2 style={{ color: 'darkblue' }}> {_.capitalize(data.name)} Turnier </h2>
+                                            <p ><strong style={{ color: 'darkblue' }}>Veranstalter:</strong> {data.organizer}</p>
+                                            <p><strong style={{ color: 'darkblue' }}>Ort:</strong> {data.location}</p>
+                                            <p><strong style={{ color: 'darkblue' }}>Datum:</strong> {humanReadableDate(data.selectedDate)}</p>
+                                            <p><strong style={{ color: 'darkblue' }}>Anzahl Mannschaften:</strong> {data.teams}</p>
                                         </div>
                                     </Link>
                                 </div>
                             </div>
-
                         </div>
                     ))}
                 </div>
